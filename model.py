44706bec9a47be1756cc44f84e3c5f5b45f942c5
Saurabh Hanwate. <127955737+Saurabhgithub1006@users.noreply.github.com>
2024-06-28 20:58:29 +0530

    Files upload

--- model.py
+++ model.py
@@ -1,15 +1,34 @@
-import joblib
+# import joblib
   
  
 
  
 
+# def get_prediction(data):
+#     model_path = r"F:\Guided Project\Git upload\New folder\Wild-Blueberry-Yield-Prediction\best_model_v0.joblib"
+#     model = joblib.load(model_path)
+#     return model.predict(data)
+    
+    
+import joblib
+import os
+
 def get_prediction(data):
-    model_path = r"best_model_v0.joblib"
-    model = joblib.load(model_path)
-    return model.predict(data)
+    model_path = os.path.join(os.path.dirname(__file__), 'best_model_v0.joblib')
     
+    if not os.path.exists(model_path):
+        raise FileNotFoundError(f"Model file not found: {model_path}")
     
- 
+    try:
+        model = joblib.load(model_path)
+    except Exception as e:
+        raise IOError(f"Error loading the model: {e}")
+    
+    try:
+        predictions = model.predict(data)
+    except Exception as e:
+        raise ValueError(f"Error making predictions: {e}")
+
+    return predictions
     
     
